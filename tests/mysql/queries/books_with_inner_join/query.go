de97474296f4e3feaadd3ca8b7fd00345823d39b
akm <akm2000@gmail.com>
2025-07-21 14:41:32 +0900

    ✅ Define books_with_inner_join for INNER JOIN

---
+++ tests/mysql/queries/books_with_inner_join/query.go
@@ -0,0 +1,21 @@
+package bookswithinnerjoin
+
+import (
+	"database/sql"
+
+	"github.com/tecowl/querybm"
+)
+
+var columns querybm.FieldMapper[Book] = querybm.NewStaticColumns(
+	[]string{"book_id", "authors.author_id", "authors.name", "isbn", "book_type", "title", "yr", "available", "tags"},
+	func(rows querybm.Scanner, book *Book) error {
+		return rows.Scan(&book.BookID, &book.AuthorID, &book.AuthorName, &book.Isbn, &book.BookType, &book.Title, &book.Yr, &book.Available, &book.Tags)
+	},
+)
+
+var sort = querybm.NewSortItem("title", false)
+
+func New(db *sql.DB, condition *Condition, pagination *querybm.Pagination) *querybm.Query[Book, *Condition, *querybm.SortItem] {
+	table := "books"
+	return querybm.New(db, condition, sort, table, columns, pagination)
+}
