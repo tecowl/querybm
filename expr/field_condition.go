2203e532247624175b5e6b446cb727a1548ad0f2
akm <akm2000@gmail.com>
2025-07-21 16:46:39 +0900

    ♻️ Extract field_conparison.go

--- expr/field_condition.go
+++ expr/field_condition.go
@@ -34,33 +34,6 @@ type FieldConditionBody interface {
 	Values() []any
 }
 
-type fieldComparison struct {
-	operator string
-	value    any
-}
-
-var _ FieldConditionBody = (*fieldComparison)(nil)
-
-func newCompare(operator string, value any) *fieldComparison {
-	return &fieldComparison{operator: operator, value: value}
-}
-func (c *fieldComparison) Build(field string) string {
-	return fmt.Sprintf("%s %s ?", field, c.operator)
-}
-func (c *fieldComparison) Values() []any { return []any{c.value} }
-
-func Eq(value any) FieldConditionBody    { return newCompare("=", value) }
-func NotEq(value any) FieldConditionBody { return newCompare("<>", value) }
-func Gt(value any) FieldConditionBody    { return newCompare(">", value) }
-func Gte(value any) FieldConditionBody   { return newCompare(">=", value) }
-func Lt(value any) FieldConditionBody    { return newCompare("<", value) }
-func Lte(value any) FieldConditionBody   { return newCompare("<=", value) }
-
-func Like(value string) FieldConditionBody           { return newCompare("LIKE", value) }
-func LikeStartsWith(value string) FieldConditionBody { return Like(value + "%") }
-func LikeEndsWith(value string) FieldConditionBody   { return Like("%" + value) }
-func LikeContains(value string) FieldConditionBody   { return Like("%" + value + "%") }
-
 type fieldInExpr struct {
 	values []any
 }
