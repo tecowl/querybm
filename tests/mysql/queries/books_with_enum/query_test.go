e6c91ab9c895fd8115d777929e24672d09c13019
akm <akm2000@gmail.com>
2025-07-21 11:14:01 +0900

    ✅ ♻️ Extract fixtures.Setup

--- tests/mysql/queries/books_with_enum/query_test.go
+++ tests/mysql/queries/books_with_enum/query_test.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"path/filepath"
 	"testing"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/stretchr/testify/assert"
@@ -16,6 +15,7 @@ import (
 
 	"github.com/tecowl/querybm"
 
+	"mysql-test/fixtures"
 	"mysql-test/models"
 )
 
@@ -55,97 +55,10 @@ func TestQuery(t *testing.T) {
 	}
 	defer db.Close()
 
-	var authors []*models.Author
 	var books []*models.Book
 
 	t.Run("Setup records", func(t *testing.T) {
-		mutation := models.New(db)
-
-		createAuthor := func(name string) *models.Author {
-			author := models.Author{Name: name}
-			res, err := mutation.CreateAuthor(ctx, name)
-			require.NoError(t, err)
-			id, err := res.LastInsertId()
-			require.NoError(t, err)
-			author.AuthorID = int32(id)
-			return &author
-		}
-
-		createBook := func(
-			authorID int32,
-			isbn string,
-			bookType models.BooksBookType,
-			title string,
-			yr int32,
-			available time.Time,
-			tags string,
-		) *models.Book {
-			res, err := mutation.CreateBook(ctx, models.CreateBookParams{
-				Isbn:      isbn,
-				BookType:  bookType,
-				Title:     title,
-				AuthorID:  authorID,
-				Yr:        yr,
-				Available: available,
-				Tags:      tags,
-			})
-			require.NoError(t, err)
-			id, err := res.LastInsertId()
-			require.NoError(t, err)
-			return &models.Book{
-				BookID:    int32(id),
-				AuthorID:  authorID,
-				Isbn:      isbn,
-				BookType:  bookType,
-				Title:     title,
-				Yr:        yr,
-				Available: available,
-				Tags:      tags,
-			}
-		}
-
-		authors = []*models.Author{
-			createAuthor("Martin Fowler"),
-			createAuthor("Kent Beck"),
-			createAuthor("Robert C. Martin"),
-			createAuthor("Uncle Bob"),
-			createAuthor("CMP Technology"),
-		}
-
-		// Books
-		books = []*models.Book{
-			// 3 books by Martin Fowler
-			createBook(authors[0].AuthorID, "978-0134757599",
-				models.BooksBookTypeHARDCOVER, "Refactoring: Improving the Design of Existing Code",
-				2018, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "refactoring, design, code"),
-			createBook(authors[0].AuthorID, "978-0321125217",
-				models.BooksBookTypeHARDCOVER, "Patterns of Enterprise Application Architecture",
-				2002, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "patterns, architecture, enterprise"),
-			createBook(authors[0].AuthorID, "978-0321125218",
-				models.BooksBookTypeHARDCOVER, "Domain-Driven Design: Tackling Complexity in the Heart of Software",
-				2004, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "domain-driven design, software architecture"),
-			createBook(authors[0].AuthorID, "978-0321213358",
-				models.BooksBookTypePAPERBACK, "Refactoring to Patterns",
-				2018, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "refactoring, design, code"),
-
-			// 2 books by Kent Beck
-			createBook(authors[1].AuthorID, "978-0321146533",
-				models.BooksBookTypeHARDCOVER, "Test-Driven Development: By Example",
-				2002, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "tdd, testing, design"),
-			createBook(authors[1].AuthorID, "978-0321278654",
-				models.BooksBookTypePAPERBACK, "Extreme Programming Explained: Embrace Change",
-				2004, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "extreme programming, agile, design"),
-
-			// 1 book by Robert C. Martin
-			createBook(authors[2].AuthorID, "978-0132350884",
-				models.BooksBookTypeHARDCOVER, "Clean Code: A Handbook of Agile Software Craftsmanship",
-				2008, time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC), "clean code, design, craftsmanship"),
-
-			// Dr. Dobb's Journal
-			createBook(authors[4].AuthorID, "0888-3076",
-				models.BooksBookTypeMAGAZINE, "Dr. Dobb's Journal",
-				1976, time.Date(1976, 1, 1, 0, 0, 0, 0, time.UTC), "programming, design, journal"),
-		}
+		_, books = fixtures.Setup(t, ctx, db)
 	})
 
 	t.Run("Query books with conditions", func(t *testing.T) {
