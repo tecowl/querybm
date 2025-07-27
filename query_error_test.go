def2b498a4e14227e4692f89f5bc80f75b82b50f
akm <akm2000@gmail.com>
2025-07-27 20:52:08 +0900

    ✅ Add test for Stmt.QueryRowContext returning error

--- query_error_test.go
+++ query_error_test.go
@@ -2,6 +2,7 @@ package querybm
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
 )
@@ -15,6 +16,50 @@ func (m *MockDB) PrepareContext(ctx context.Context, query string) (Stmt, error)
 		return m.PrepareContextFunc(ctx, query)
 	}
 	return nil, nil // nolint:nilnil
+}
+
+type MockStmt struct {
+	close           func() error
+	queryRowContext func(ctx context.Context, args ...any) Row
+	queryContext    func(ctx context.Context, args ...any) (Rows, error)
+}
+
+var _ Stmt = (*MockStmt)(nil)
+
+func (m *MockStmt) Close() error {
+	if m.close != nil {
+		return m.close()
+	}
+	return nil
+}
+
+func (m *MockStmt) QueryContext(ctx context.Context, args ...any) (Rows, error) {
+	if m.queryContext != nil {
+		return m.queryContext(ctx, args...)
+	}
+	return nil, nil // nolint:nilnil
+}
+
+func (m *MockStmt) QueryRowContext(ctx context.Context, args ...any) Row {
+	if m.queryRowContext != nil {
+		return m.queryRowContext(ctx, args...)
+	}
+	return nil
+}
+
+type MockRow struct {
+	err  error
+	scan func(dest ...any) error
+}
+
+func (m *MockRow) Err() error {
+	return m.err
+}
+func (m *MockRow) Scan(dest ...any) error {
+	if m.scan != nil {
+		return m.scan(dest...)
+	}
+	return nil // nolint:nilnil
 }
 
 func TestQueryPrepareContextError(t *testing.T) {
@@ -47,4 +92,41 @@ func TestQueryPrepareContextError(t *testing.T) {
 			t.Error("Expected error, got nil")
 		}
 	})
+}
+
+func TestStmtQueryRowContextError(t *testing.T) {
+	t.Parallel()
+
+	scanErrors := []error{
+		sql.ErrNoRows,
+		fmt.Errorf("scan error"),
+	}
+
+	for _, scanErr := range scanErrors {
+		t.Run(scanErr.Error(), func(t *testing.T) {
+			t.Parallel()
+
+			row := &MockRow{
+				err: fmt.Errorf("runtime row error"), // nolint:err113,perfsprint
+				scan: func(dest ...any) error {
+					return fmt.Errorf("scan error") // nolint:err113,perfsprint
+				},
+			}
+			stmt := &MockStmt{
+				queryRowContext: func(ctx context.Context, args ...any) Row { return row },
+			}
+			db := &MockDB{
+				PrepareContextFunc: func(context.Context, string) (Stmt, error) { return stmt, nil },
+			}
+
+			q := &Query[any]{
+				db:     db,
+				Table:  "users",
+				Fields: NewFields[any]([]string{"id", "name"}, nil),
+			}
+			if _, err := q.Count(t.Context()); err == nil {
+				t.Error("Expected error, got nil")
+			}
+		})
+	}
 }
