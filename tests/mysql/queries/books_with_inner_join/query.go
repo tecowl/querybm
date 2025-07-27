4ffee0b5e03c0f9300226a0c71c2899537050e4d
akm <akm2000@gmail.com>
2025-07-27 11:28:18 +0900

    ✅ Merge files to query.go in tests/mysql/queries/books_with_inner_join

--- tests/mysql/queries/books_with_inner_join/query.go
+++ tests/mysql/queries/books_with_inner_join/query.go
@@ -2,11 +2,37 @@ package bookswithinnerjoin
 
 import (
 	"database/sql"
+	"mysql-test/models"
 
 	"github.com/tecowl/querybm"
+	. "github.com/tecowl/querybm/expr"
 )
 
-func addAuthorsJoin(s *querybm.Statement) {
+type Book struct {
+	models.Book
+	AuthorName string
+}
+
+func New(db *sql.DB, condition *Condition, pagination *querybm.Pagination) *querybm.Query[Book] {
+	return querybm.New(db, "books", columns, condition,
+		querybm.NewSortItem("title", false), pagination,
+	)
+}
+
+type Condition struct {
+	AuthorName string
+}
+
+var _ querybm.Condition = (*Condition)(nil)
+
+func (c *Condition) Build(s *querybm.Statement) {
+	if c.AuthorName != "" {
+		innerJoinAuthors(s)
+		s.Where.Add(Field("authors.name", LikeContains(c.AuthorName)))
+	}
+}
+
+func innerJoinAuthors(s *querybm.Statement) {
 	s.Table.InnerJoin("authors", "books.author_id = authors.author_id")
 }
 
@@ -15,12 +41,5 @@ var columns querybm.FieldMapper[Book] = querybm.NewFields(
 	func(rows querybm.Scanner, book *Book) error {
 		return rows.Scan(&book.BookID, &book.AuthorID, &book.AuthorName, &book.Isbn, &book.BookType, &book.Title, &book.Yr, &book.Available, &book.Tags)
 	},
-	addAuthorsJoin,
+	innerJoinAuthors,
 )
-
-var sort = querybm.NewSortItem("title", false)
-
-func New(db *sql.DB, condition *Condition, pagination *querybm.Pagination) *querybm.Query[Book] {
-	table := "books"
-	return querybm.New(db, table, columns, condition, sort, pagination)
-}
